8f5011a431cb713c9b790857c17f8885cd8c6366
ciherrera20 <ciherrera20@icloud.com>
2022-12-23 21:40:40 -0800

    Edited the exploration notebook

--- notebooks/common.py
+++ notebooks/common.py
@@ -2,6 +2,8 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 from datetime import datetime, timedelta
+import os
+import re
 
 def get_shark_gdf(filename):
     '''Load shark position data into a GeoDataFrame'''
@@ -15,3 +17,47 @@ def get_shark_gdf(filename):
 
 def wrap_to_pi(theta):
     return ((theta - np.pi) % (2 * np.pi)) - np.pi
+
+def find_files(*sources, name=None, extension=None):
+    '''
+    Recursively retrieve files from the given source directories whose names and/or extensions (full)match the given patterns.
+    name: string or regex pattern
+    extension: string or regex pattern
+    Returns a DirEntry generator
+    '''
+    # Compile regexes if needed
+    if name is None:
+        name = re.compile(r'.*')
+    elif type(name) is not re.Pattern:
+        name = re.compile(name)
+    if extension is None:
+        extension = re.compile(r'.*')
+    elif type(extension) is not re.Pattern:
+        extension = re.compile(extension)
+
+    # Keep track of the sources already scanned and the files already found
+    memo_table = {}
+
+    def find_files_helper(*sources):
+        # Search through each source directoty
+        for source in sources:
+            # Get all of the contents of the source directory and search them
+            entries = os.scandir(source)
+            for entry in entries:
+                # Check if the entry has already been scanned or matched
+                normed = os.path.normpath(entry.path)
+                if normed not in memo_table:
+                    memo_table[normed] = True
+                    # If the current entry is itself a directory, search it recursively
+                    if entry.is_dir():
+                        yield from find_files_helper(entry)
+
+                    # Otherwise yield entries whose name matches the name pattern and whose extension matches the extension pattern
+                    else:
+                        # Return only entries that have not already been found
+                        filename, fileext = os.path.splitext(entry.name)
+                        if name.fullmatch(filename) is not None and \
+                        extension.fullmatch(fileext) is not None:
+                            yield entry
+            entries.close()
+    return find_files_helper(*sources)
